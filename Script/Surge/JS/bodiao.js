d037736971bdfd73845919ff313b1442e714f94b
林夕 <139839847+Linqinxi@users.noreply.github.com>
2023-12-11 01:21:59 +0800

    Create bodiao.js

---
+++ Script/Surge/JS/bodiao.js
@@ -0,0 +1,5 @@
+var obj = JSON.parse($response.body);
+
+obj.data.moduleList = obj.data.moduleList.filter(item => item.name !== "轮播图" && item.name !== "波点实验室");
+
+$done({body : JSON.stringify(obj)});
