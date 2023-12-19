62fb12ea55d42ac91e8ed15d45b0bc586ce8c99e
Linqinxi <linxinyaaa@126.com>
2023-12-19 17:23:44 +0800

    波点

--- Script/JS/bodian.js
+++ Script/JS/bodian.js
@@ -1,7 +1,22 @@
 #!name=波点音乐
 
-var obj = JSON.parse($response.body);
+var body = $response.body;
+var obj = JSON.parse(body);
+const ad = /api\/service\/(home\/index|banner\/myPage)/;
+const adv = /api\/play\/(advert\/info|listening\/entrance\/music)/;
+const to = /api\/search\/topic\/word\/list/;
 
-obj.data.moduleList = obj.data.moduleList.filter(item => item.name !== "轮播图" && item.name !== "波点实验室");
+if (ad.test($request.url)) {
+   obj.data.bannerList = [];
+  obj.data.moduleList = obj.data.moduleList.filter(item => item.name !== "轮播图" && item.name !== "波点实验室");
+}
 
-$done({body : JSON.stringify(obj)});
+if (adv.test($request.url)) {
+   obj.data.entranceMusic = {};
+   obj.data.advert = {};
+}
+if (to.test($request.url)) {
+   obj.data = {};
+}
+
+$done({ body: JSON.stringify(obj) });
