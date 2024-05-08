308d867392ecc0e214cb686f78850371f3c630ae
Linqinxi <linxinyaaa@126.com>
2024-05-09 02:19:08 +0800

    更新kdxz.js

--- Script/JS/kdxz.js
+++ Script/JS/kdxz.js
@@ -1,5 +1,7 @@
 #!name=口袋写作
- var linxi = JSON.parse($response.body);
-linxi.data.vipTime=1850170708000;
-$done({ body: JSON.stringify(linxi) });
+ var Linxi = JSON.parse($response.body);
+Linxi.data.vipTime = 1850170708000;
+$done({
+  body: JSON.stringify(Linxi)
+});
 
